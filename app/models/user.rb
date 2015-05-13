f3e8f2c867747b2538cb750666c60343dfca3461
Greg Dziemidowicz <greg@cogent.co>
2015-05-13 17:46:51 +1000

    trusting facebook with user e-mail...

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,9 +20,11 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(email: auth.info.email).first_or_create do |user|
+      user.provider= auth.provider
+      user.uid = auth.uid
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0,20] unless user.password.present?
     end
   end
 
